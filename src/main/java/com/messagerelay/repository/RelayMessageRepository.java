447c221feed8071f961a2cf8ae2b3463512c8488
Calvin Power <calvinpower44@gmail.com>
2026-09-07 16:51:07 +0100

    Add durable message repository contract

--- src/main/java/com/messagerelay/repository/RelayMessageRepository.java
+++ src/main/java/com/messagerelay/repository/RelayMessageRepository.java
@@ -2,7 +2,18 @@ package com.messagerelay.repository;
 
 import com.messagerelay.domain.RelayMessage;
 
+import java.util.List;
+
 public interface RelayMessageRepository {
 
-    void save(RelayMessage message);
+    void save(
+            RelayMessage message
+    );
+
+    boolean delete(
+            String recipientId,
+            String messageId
+    );
+
+    List<RelayMessage> findAllPending();
 }
